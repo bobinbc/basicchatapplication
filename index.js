4b3c83931a2c390f23063951bf80b483ece7a093
bobinbc <bobinbc@gmail.com>
2015-01-20 16:54:41 -0500

    Updated to student names

--- index.js
+++ index.js
@@ -2,10 +2,14 @@ var express = require('express'),
     app = express();
 var server = require('http').createServer(app);
 //var io = require('socket.io')(server);
-var port = process.env.PORT || 8180;
+var port = process.env.PORT || 8080;
 
 server.listen(port,function(){
    console.log('I\'m listening to port: %d', port);
+});
+
+app.get("/student/:id", function(req,res){
+    res.end(req.params.id + " rocks!");
 });
 
 app.use(express.static(__dirname+ '/www/'));
